d3ffd816e04de69761fe393f1537c42d0511c935
Admer <49600504+Admer456@users.noreply.github.com>
2020-11-15 17:49:51 +0100

    Building: Prevent failure if targetPath doesn't exist

--- filecopy.bat
+++ filecopy.bat
@@ -1,10 +1,17 @@
 rem https://www.sourcemodding.com/tutorials/goldsrc/projects/where-is-poppy/setting-up-a-goldsrc-mod/#Modify_the_Post_Build_Event
 @echo on
- 
+
 set "sourcePath=%~f1"
 set "targetPath=%~f2"
- 
-echo "SourcePath : %sourcePath%"
-echo "TargetPath : %targetPath%"
- 
+
+rem Prevents the compile from failing if the target path doesn't exist
+rem Brushtool did not account for this, it seems
+if not exist "%targetPath%" (
+	echo "filecopy :: targetPath does not exist, you'll need to copy binaries manually"
+	exit 0
+)
+
+echo "filecopy :: SourcePath : %sourcePath%"
+echo "filecopy :: TargetPath : %targetPath%"
+
 @copy "%sourcePath%" "%targetPath%"
